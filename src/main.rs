59dab9929a8ca20c2f776048df97e79f09249679
Max <max.hammer1998@googlemail.com>
2024-04-15 18:44:12 +0200

    add robustness to input checking

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-use std::{char::MAX, io};
+use std::io;
 use rand::{thread_rng, Rng};
-use std::cmp::Ordering;
-use std::fs::{read, File};
+use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
 const MAX_ATTEMPTS: i32 = 5;
 
+fn count_characters(input: &str) -> usize {
+    input.chars().count()
+}
+
 fn read_word_file(path: String) -> Result<Vec<String>> {
-    // Open the file for reading
     let file: File = File::open(path)?;
-
-    // Create a buffered reader to read the file
     let buffered: BufReader<File> = BufReader::new(file);
-
-    // Create a vector to hold the words.
     let mut words: Vec<String> = Vec::new();
 
-    // Read the file line by line using the lines() iterator from BufReader.
     for line in buffered.lines() {
-        // For each line, which is returned as a Result<String>, add it to the vector.
         let line: String = line?;
         words.push(line);
     }
@@ -42,30 +38,40 @@ fn pick_word(words: Result<Vec<String>>) -> String{
 }
 
 fn main() {
-
     let words: Result<Vec<String>> = read_word_file(String::from("words.txt"));
     let secret_word: String = pick_word(words);
 
     println!("----------- Wordle -----------");
     println!("The secret word is '{secret_word}'");
     println!("Guess the word!");
-    println!("------------------------------");
 
     let mut attempt: i32 = 1;
 
     while attempt <= MAX_ATTEMPTS {
-        println!("Guess {attempt}/{MAX_ATTEMPTS}");
+        println!("--------------- {attempt}/{MAX_ATTEMPTS} ---------------");
 
         let mut guess: String = String::new();
-    
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
 
-        let guess: String = match guess.trim().parse() {
-            Ok(guess) => guess,
-            Err(_) => continue,
-        };
+        loop {
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
+
+            guess = match guess.trim().to_lowercase().parse() {
+                Ok(guess) => guess,
+                Err(_) => continue,
+            };
+
+            let len_guess = count_characters(&guess);
+
+            // wordle only contains 5 character words
+            if len_guess == 5 {
+                break;
+            }
+
+            guess.clear();
+            println!("Your word has {len_guess} characters. Type a word with 5 characters.");
+        }
 
         if guess == secret_word {
             println!("You guessed right. You win!");
@@ -75,4 +81,5 @@ fn main() {
         }
         attempt = attempt + 1;
     }
+    println!("You ran out of attempts. Try again.");
 }
